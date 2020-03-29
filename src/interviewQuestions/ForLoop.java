352c5a6e4bc1d17e6bebe32100646434d949bb1e
Sofiya <Sofiya@Sofiya-PC>
2020-03-29 10:22:59 +0530

    Only one file i added. Commiting to Branch Husain.

---
+++ src/interviewQuestions/ForLoop.java
@@ -0,0 +1,19 @@
+package interviewQuestions;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+         for(int i=0;i<=5;i++)
+         {
+        	 for(int j=0;j<=5;j++)
+        	 {	 
+        		 
+        		 System.out.println("Mohd Husain Ahmad");
+		
+	}
+         }
+
+
+}
+}
